67111ceae2e1d0481a7de9cd356adf98df7ceffa
cwarim-A <akoladequam3@gmail.com>
2025-09-01 17:15:31 +0100

    updated the route file

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,65 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { LandingPage } from "./screens/LandingPage/LandingPage";
-import Router from "./Router";
+import { AboutPage } from "./screens/AboutPage/AboutPage";
+import { Team } from "./screens/Team/Team";
+import { Blog } from "./screens/Blog/Blog";
+import { Faq } from "./screens/FAQ/Faq";
+import { Contact } from "lucide-react";
+import { ContactPage } from "./screens/Contact/ContactPage";
+import { Social } from "./screens/Social/Social";
+import { PrivacyPolicy } from "./screens/Policy/PrivacyPolicy";
+import { TermsAndConditions } from "./screens/Terms/TermsAndConditions";
+
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LandingPage />
+  },
+  {
+    path: "/about-us",
+    element: <AboutPage />
+  },
+  {
+    path: "/team",
+    element: <Team />
+  },
+  {
+    path: "/blog",
+    element: <Blog />
+  },
+  {
+    path: "/faq",
+    element: <Faq />
+  },
+  {
+    path: "/contact",
+    element: <Contact />
+  },
+  {
+    path: "/contact-page",
+    element: <ContactPage />
+  },
+  {
+    path: "/social",
+    element: <Social />
+  },
+  {
+    path: "/privacy-policy",
+    element: <PrivacyPolicy />
+  },
+  {
+    path: "/terms-and-conditions",
+    element: <TermsAndConditions />
+  }
+])
 
 createRoot(document.getElementById("app") as HTMLElement).render(
   <StrictMode>
-    <Router />
+    <RouterProvider router={router} />
   </StrictMode>,
 );
